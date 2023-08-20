0690fa6116a8efce515394d90495937a7b9e1dd5
shroomwastaken <xeoplay123456@gmail.com>
2023-08-20 22:34:20 +0300

    0.0.2

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Missing Translation Save Manipulation Tool
 This is a tool that is used to manipulate the save data of Missing Translation (2015).
+This tool only works on Windows.
 
 # Changelog
 0.0.1
 - Initial Commit
 - Save deletion added (useful for beginning new game and resetting speedruns)
+
+0.0.2
+- Added automatic closing and reopening of the game.
+- Added automatic save path retrieval.
 
 # Installation
 Download MTSMT.exe file from the Releases tab.
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from tkinter import Tk, Button, Label, W, filedialog, messagebox
 from tkinter.font import Font
-import shutil
+from os import getenv
+from shutil import rmtree
+from subprocess import call
+from time import sleep
+import webbrowser
+
 
 window = Tk()
-path_to_save = ""
+
+path_to_save = getenv("APPDATA") + "\..\Local\MissingTranslation"
 
 window.title("Missing Translation Save Manipulation Tool")
 window.resizable(False, False)
@@ -11,41 +17,28 @@ window.configure(background = "#04819E")
 
 cascadia_font = Font(family = "Cascadia Code", size = 18)
 
-cur_path_lbl = Label(text = "Current path: ", font = cascadia_font)
+cur_path_lbl = Label(text = f"Current path: {path_to_save}", font = cascadia_font)
 cur_path_lbl.configure(background = "#04819E")
 cur_path_lbl.grid(column = 0, row = 2, sticky = W, columnspan = 2)
 
-def pick_path():
-    global path_to_save, cur_path_lbl
-    prev_path = path_to_save
-    path_to_save = filedialog.askdirectory()
-    if "MissingTranslation" not in path_to_save and path_to_save != "":
-        messagebox.showerror(title = "Error!", message = "Invalid path!")
-        path_to_save = prev_path
-    else:
-        cur_path_lbl.configure(text = f"Current path: {path_to_save}")
-        cur_path_lbl.update()
-
 def del_save():
     global path_to_save
+    call(["taskkill","/F","/IM","nw.exe"])
+
+    sleep(.5)
+
     try:
-        shutil.rmtree(path_to_save)
+        rmtree(path_to_save)
     except FileNotFoundError:
-        messagebox.showerror(title = "Error!", message = "Empty/Invalid path!")
-        return
-    except PermissionError:
-        messagebox.showerror(title = "Erorr!", message = "Please close the game before manipulating the save!")
+        messagebox.showerror(title = "Error!", message = "Save doesn't exist!")
         return
     
     messagebox.showinfo(title = "Success!", message = "Save deleted!")
+    webbrowser.open("steam://rungameid/395520")
 
 title = Label(text = "Missing Translation SMT", font = ("Cascadia Code", 36))
 title.configure(background = "#04819E")
 title.grid(column = 0, row = 0, columnspan= 2)
-
-pick_path_btn = Button(text = "Pick save path", font = cascadia_font, command = pick_path)
-pick_path_btn.configure(background = "#04819E")
-pick_path_btn.grid(column = 1, row = 1)
 
 del_save_btn = Button(text = "Delete save", font = cascadia_font, command = del_save)
 del_save_btn.configure(background = "#04819E")
